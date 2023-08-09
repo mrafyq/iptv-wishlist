70235a4657518a7d6205a3d1aa67173c95e17fb7
Mohamed RAFYQ <muhamedrafyq@gmail.com>
2023-08-09 12:57:19 +0100

    manage add new wishlit

--- live.js
+++ live.js
@@ -193,13 +193,37 @@ document.addEventListener('keyup', function (e) {
 
 });
 
+// Add new Fav
+var buttonAddFavoris = document.querySelector('.action-add');
+var buttonAddFavSave = document.querySelector('.action-add-favoris-save');
+var inputAdd = document.querySelector('.popup #add');
+
+// Rename
 var buttonRenameFavoris = document.querySelector('.action-rename');
 var buttonRenameCancel = document.querySelector('.action-cancel');
 var popupRename = document.querySelector('.popup.popup-rename');
 var buttonRenameSave = document.querySelector('.action-rename-save');
 var inputRename = document.querySelector('.popup #rename');
+var popupFav = document.querySelector('.popup.popup-add-wishlist');
 
+buttonAddFavoris.addEventListener('click', () => {
+    popupFav.style.display = 'flex';
+})
 
+buttonAddFavSave.addEventListener('click', () => {
+    fetch('data/db.json')
+        .then(res => res.json())
+        .then(data => {
+            data.favoris.push({
+                "favoris_id": 100,
+                "favoris_name": inputAdd.value,
+                "order": data.favoris.length,
+                "channels": []
+            });
+            console.log(data.favoris);
+        })
+        .catch(err => console.log(err));
+})
 
 buttonRenameFavoris.addEventListener('click', (event) => {
     var favorisSelected = document.querySelector('.list-favoris .favoris.selected');
